1d61460f6f45dfa0ee28e58baf5125740e73e8d6
EduPoloRuiz <eduardo.polo@solera.com>
2020-08-28 14:02:50 +0200

    Cambios en metodos turnOn y check

--- Christmas/src/main/java/com/christmasLights/Panel/Panel.java
+++ Christmas/src/main/java/com/christmasLights/Panel/Panel.java
@@ -2,9 +2,9 @@ package com.christmasLights.Panel;
 
 import lombok.Builder;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Set;
+import java.util.List;
 
 @Builder
 public class Panel {
@@ -19,21 +19,26 @@ public class Panel {
         return this.isCreated;
     }
 
-    public Boolean checkLights() {
+    public Boolean checkLights(Integer[][] coordinatesLights) {
+        final List<Integer> lista = new ArrayList<Integer>();
         for(int i = 0; i < this.coordinatesPanel.length ; i++){
             for(int j = 0; j < this.coordinatesPanel[i].length  ; j++){
-                if(!this.coordinatesPanel[i][j]){
-                    return false;
+                if(this.coordinatesPanel[i][j]){
+                    lista.add(i);
+                    lista.add(j);
                 }
             }
         }
-        return true;
+        System.out.println(lista.size()/2);
+        return !lista.isEmpty();
     }
 
-    public void turnOn() {
-        for(int i = 0; i < this.coordinatesPanel.length ; i++){
-            for(int j = 0; j < this.coordinatesPanel[i].length  ; j++){
-                this.coordinatesPanel[i][j] = true;
+    public void turnOn(Integer[][] coordinatesLights) {
+        for (int m = 0; m < coordinatesLights.length ; m++) {
+            for (int i = coordinatesLights[m][0]; i <= coordinatesLights[m][2]; i++) {
+                for (int j = coordinatesLights[m][1]; j <= coordinatesLights[m][3]; j++) {
+                    this.coordinatesPanel[i][j] = true;
+                }
             }
         }
     }
--- Christmas/src/test/java/com/christmasLights/Panel/PanelTest.java
+++ Christmas/src/test/java/com/christmasLights/Panel/PanelTest.java
@@ -1,6 +1,5 @@
 package com.christmasLights.Panel;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -22,17 +21,25 @@ public class PanelTest {
     }
     
     @Test
-    void should_returnTrue_when_lightsAreTurnedOn(){
+    void should_returnTrue_when_lightsAreCheckedOn(){
         //given
         final Panel panel = Panel.builder().build();
         panel.create(1000);
-        panel.turnOn();
-        //when
+        final Integer[][] coordinatesLights = new Integer[2][4];
+        coordinatesLights[0][0] = 0;
+        coordinatesLights[0][1] = 0;
+        coordinatesLights[0][2] = 2;
+        coordinatesLights[0][3] = 2;
+        coordinatesLights[1][0] = 3;
+        coordinatesLights[1][1] = 3;
+        coordinatesLights[1][2] = 4;
+        coordinatesLights[1][3] = 4;
 
-        final Boolean checkLightsAreTurnedOn = panel.checkLights();
+        //when
+        panel.turnOn(coordinatesLights);
+        final Boolean checkLightsAreTurnedOn = panel.checkLights(coordinatesLights);
 
         //then
         assertThat(checkLightsAreTurnedOn).isTrue();
-
     }
 }
